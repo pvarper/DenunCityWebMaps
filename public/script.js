ea019deab70b2e1fe06731bc46c5fe3d6d875409
pvarper <p.varper@gmail.com>
2023-04-15 22:52:40 -0400

    feat(master): map load, draw marker feat(master): fix map load, draw marker

--- public/script.js
+++ public/script.js
@@ -1,37 +1,82 @@
 // Initialize and add the map
 /*global google*/
 let map;
-let drawingManager;
+
+let markers = [];
 function initMap() {
     // eslint-disable-next-line no-undef
-
+    const santacruz = {lat: -17.78629, lng: -63.18117};
     map = new google.maps.Map(document.getElementById("map"), {
-        center: {lat: -17.78629, lng: -63.18117},
+        center: santacruz,
         zoom: 15
-    })
-
-
-    drawingManager = new google.maps.drawing.DrawingManager({
-        drawingControlOptions : {
-            drawingModes:['marker']
-        }
     });
-    drawingManager.setMap(map);
-    let markers= new google.maps.MVCArray();
-    google.maps.event.addListener(drawingManager,'overlaycomplete',function (event){
-        if(event.type == 'marker'){
-            const position = event.overlay.position;
 
-            console.log(position.lat(),position.lng());
-            markers.push(new google.maps.LatLng(position.lat(),position.lng()));
+    let markersPoligon= new google.maps.MVCArray();
+    map.addListener("click", (event) => {
+        addMarker(event.latLng);
+        console.log(event.latLng.lat(),event.latLng.lng());
+        markersPoligon.push(new google.maps.LatLng(event.latLng.lat(),event.latLng.lng()));
+        let polygonOptions = {path: markersPoligon};
+        let polygon = new google.maps.Polygon(polygonOptions);
+        polygon.setMap(map);
+    });
 
-        }
-     let polygonOptions = {path: markers};
-     let polygon = new google.maps.Polygon(polygonOptions);
-     polygon.setMap(map);
-    })
 
+
+    //add event listeners for the buttons
+    document
+        .getElementById("show-markers")
+        .addEventListener("click", showMarkers);
+    document
+        .getElementById("hide-markers")
+        .addEventListener("click", hideMarkers);
+    document
+        .getElementById("delete-markers")
+        .addEventListener("click", deleteMarkers);
+    //Adds a marker at the center of the map.
 
 }
 
-initMap();
+function drawPoligon(){
+
+
+    let polygon = new google.maps.Polygon({
+        paths:  {path: markers},
+
+    });
+    polygon.setMap(map);
+}
+
+function addMarker(position) {
+    const marker = new google.maps.Marker({
+        position,
+        map,
+    });
+    markers.push(marker);
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+    for (let i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+
+    }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function hideMarkers() {
+    setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+    setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+    hideMarkers();
+    markers = [];
+}
+
+window.initMap = initMap;
