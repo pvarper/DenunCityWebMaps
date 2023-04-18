bb07dc8b903df788270ef3c2d652b49ae61565e7
pvarper <p.varper@gmail.com>
2023-04-18 11:17:39 -0400

    feat(master): map load, draw marker feat(master): fix map load, draw marker feat(master): fix draw, map load, draw marker feat(master): create script2 with drawing polygon and delete
    feat(master): fix delete drawing
    feat(master): fix  drawing
    feat(master): fix  drawing 2

--- public/script.js
+++ public/script.js
@@ -5,6 +5,7 @@ let map;
 let markers = [];
 let markersPoligon= [];
 let polygon;
+let listaPolygon=[];
 
 
 function initMap() {
@@ -17,36 +18,50 @@ function initMap() {
 
 
     map.addListener("click", (event) => {
-        markersPoligon.push(new google.maps.LatLng(event.latLng.lat(),event.latLng.lng()));
-        sorted_markers();
+        //markersPoligon.push(new google.maps.LatLng(event.latLng.lat(),event.latLng.lng()));
+        const coordi = {
+
+            "lat": parseFloat(event.latLng.lat()),
+            "lng": parseFloat(event.latLng.lng())
+        }
+        markersPoligon.push(coordi);
         addMarker(event.latLng);
-        drawPoligon();
+
     });
 
 
     document
+        .getElementById("draw-polygon")
+        .addEventListener("click", drawPoligon);
+    document
         .getElementById("delete-markers")
         .addEventListener("click", deleteMarkers);
-    //Adds a marker at the center of the map.
 
 }
 function drawPoligon(){
-    let polygonOptions = {
-        path: markersPoligon,
-        strokeColor: "#7CFC00",
+    //console.log(markersPoligon);
+    var listaOrdenada= ordenarCoordenadas(markersPoligon);
+        let polygonOptions = {
+        path: listaOrdenada,
+        //strokeColor: "#7CFC00",
     };
     polygon = new google.maps.Polygon(polygonOptions);
+    listaPolygon.push(polygon);
     polygon.setMap(map);
-
+    markersPoligon=[];
 }
-const imgicon = "/img/green.png";
+
+
+
+//const imgicon = "/img/green.png";
 function addMarker(position) {
     const marker = new google.maps.Marker({
         position,
-        icon: imgicon,
+        //icon: imgicon,
         map,
     });
     markers.push(marker);
+
 }
 
 // Sets the map on all markers in the array.
@@ -61,8 +76,56 @@ function setMapOnAll(map) {
 function deleteMarkers() {
     setMapOnAll(null);
     markers = [];
+    listaPolygon.map(ele =>{
+        ele.setMap(null);
+    });
+    // polygon.setMap(null);
+    markersPoligon=[];
 
 }
 
+function ordenarCoordenadas(coordenadas){
+    // Definir la lista de coordenadas
+    // let coordenadas = [
+    //     { latitud: 40.7128, longitud: -74.0060 },
+    //     { latitud: 37.7749, longitud: -122.4194 },
+    //     { latitud: 41.8781, longitud: -87.6298 },
+    //     { latitud: 34.0522, longitud: -118.2437 },
+    // ];
 
+// Encontrar el centroide de la figura
+    let centroide = { lat: 0, lng: 0 };
+    for (let i = 0; i < coordenadas.length; i++) {
+        centroide.lat += coordenadas[i].lat;
+        centroide.lng += coordenadas[i].lng;
+    }
+    centroide.lat /= coordenadas.length;
+    centroide.lng /= coordenadas.length;
+
+// Calcular los ángulos entre cada punto y el centroide
+    for (let i = 0; i < coordenadas.length; i++) {
+        let vector1 = {
+            lat: coordenadas[i].lat - centroide.lat,
+            lng: coordenadas[i].lng - centroide.lng,
+        };
+        let vector2 = { lat: 1, lng: 0 }; // Vector de referencia en el meridiano de Greenwich
+        let producto_cruzado =
+            vector1.lat * vector2.lng - vector1.lng * vector2.lat;
+        let producto_punto =
+            vector1.lat * vector2.lat + vector1.lng * vector2.lng;
+        coordenadas[i].angulo = Math.atan2(producto_cruzado, producto_punto);
+    }
+
+// Ordenar los puntos en función de sus ángulos
+    coordenadas.sort(function (a, b) {
+        return a.angulo - b.angulo;
+    });
+
+// Mostrar los puntos ordenados en la consola
+//     console.log("Coordenadas ordenadas:");
+//     for (let i = 0; i < coordenadas.length; i++) {
+//         console.log(`(${coordenadas[i].lat}, ${coordenadas[i].lng})`);
+//     }
+    return coordenadas;
+}
 window.initMap = initMap;
